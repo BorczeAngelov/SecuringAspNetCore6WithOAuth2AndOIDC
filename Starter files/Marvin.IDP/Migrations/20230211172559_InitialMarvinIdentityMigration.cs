1932f805109b047708990b9772d91d3d16b77027
Borcze Angelov <Borcze.Angelov@yahoo.com>
2023-02-11 18:31:13 +0100

    create user database and initial migration

---
+++ Starter files/Marvin.IDP/Migrations/20230211172559_InitialMarvinIdentityMigration.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Marvin.IDP.Migrations
+{
+    public partial class InitialMarvinIdentityMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Subject = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    UserName = table.Column<string>(type: "TEXT", maxLength: 200, nullable: true),
+                    Password = table.Column<string>(type: "TEXT", maxLength: 200, nullable: true),
+                    Active = table.Column<bool>(type: "INTEGER", nullable: false),
+                    ConcurrencyStamp = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserClaims",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Type = table.Column<string>(type: "TEXT", maxLength: 250, nullable: false),
+                    Value = table.Column<string>(type: "TEXT", maxLength: 250, nullable: false),
+                    ConcurrencyStamp = table.Column<string>(type: "TEXT", nullable: true),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserClaims_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Active", "ConcurrencyStamp", "Password", "Subject", "UserName" },
+                values: new object[] { new Guid("13229d33-99e0-41b3-b18d-4f72127e3971"), true, "81181579-0ea4-4b81-a849-5d38660a2855", "password", "d860efca-22d9-47fd-8249-791ba61b07c7", "David" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Active", "ConcurrencyStamp", "Password", "Subject", "UserName" },
+                values: new object[] { new Guid("96053525-f4a5-47ee-855e-0ea77fa6c55a"), true, "b98047c3-574b-4ff3-9ea3-657fab6873b7", "password", "b7539694-97e7-4dfe-84da-b4256e1ff5c7", "Emma" });
+
+            migrationBuilder.InsertData(
+                table: "UserClaims",
+                columns: new[] { "Id", "ConcurrencyStamp", "Type", "UserId", "Value" },
+                values: new object[] { new Guid("0a209b3a-7b53-42db-8763-8caaf9672e6b"), "c15e16e2-4662-4948-96d3-fd84a67fbad0", "given_name", new Guid("13229d33-99e0-41b3-b18d-4f72127e3971"), "David" });
+
+            migrationBuilder.InsertData(
+                table: "UserClaims",
+                columns: new[] { "Id", "ConcurrencyStamp", "Type", "UserId", "Value" },
+                values: new object[] { new Guid("0ad70af1-4088-4b81-a3a2-10811bb8273c"), "79fa7b6f-33a5-4fb0-beb4-b69f07d2f3b6", "country", new Guid("13229d33-99e0-41b3-b18d-4f72127e3971"), "nl" });
+
+            migrationBuilder.InsertData(
+                table: "UserClaims",
+                columns: new[] { "Id", "ConcurrencyStamp", "Type", "UserId", "Value" },
+                values: new object[] { new Guid("25fd0f9e-68b5-4668-81c3-c97e74dafecc"), "819f1c06-8912-4bc6-b6c5-50b7ebc2bbba", "family_name", new Guid("96053525-f4a5-47ee-855e-0ea77fa6c55a"), "Flagg" });
+
+            migrationBuilder.InsertData(
+                table: "UserClaims",
+                columns: new[] { "Id", "ConcurrencyStamp", "Type", "UserId", "Value" },
+                values: new object[] { new Guid("2c0c1ede-a24f-4e56-8f60-b710234822e8"), "4ae51a29-1496-4a50-8d69-1c15831eccd5", "role", new Guid("13229d33-99e0-41b3-b18d-4f72127e3971"), "FreeUser" });
+
+            migrationBuilder.InsertData(
+                table: "UserClaims",
+                columns: new[] { "Id", "ConcurrencyStamp", "Type", "UserId", "Value" },
+                values: new object[] { new Guid("98f951fb-d404-49e7-a2d0-10180da701f1"), "e883c9ff-d8bd-4ed3-8308-952fb38c7b12", "role", new Guid("96053525-f4a5-47ee-855e-0ea77fa6c55a"), "PayingUser" });
+
+            migrationBuilder.InsertData(
+                table: "UserClaims",
+                columns: new[] { "Id", "ConcurrencyStamp", "Type", "UserId", "Value" },
+                values: new object[] { new Guid("d10a19e1-864a-4959-992f-b4bb95a3558d"), "39379964-5bac-488a-ba42-be0f33e0798b", "family_name", new Guid("13229d33-99e0-41b3-b18d-4f72127e3971"), "Flagg" });
+
+            migrationBuilder.InsertData(
+                table: "UserClaims",
+                columns: new[] { "Id", "ConcurrencyStamp", "Type", "UserId", "Value" },
+                values: new object[] { new Guid("da9c9f6b-6cfb-493a-b6e4-3ecf5b003517"), "aa86e02b-06eb-404a-a1d3-52013da41416", "given_name", new Guid("96053525-f4a5-47ee-855e-0ea77fa6c55a"), "Emma" });
+
+            migrationBuilder.InsertData(
+                table: "UserClaims",
+                columns: new[] { "Id", "ConcurrencyStamp", "Type", "UserId", "Value" },
+                values: new object[] { new Guid("fbb22ffc-d2d1-4ee7-a7f3-f0dbe64bb75a"), "8f5de25c-c093-42b1-af8a-da11135e977e", "country", new Guid("96053525-f4a5-47ee-855e-0ea77fa6c55a"), "be" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserClaims_UserId",
+                table: "UserClaims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Subject",
+                table: "Users",
+                column: "Subject",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserName",
+                table: "Users",
+                column: "UserName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserClaims");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
